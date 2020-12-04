d513f19f50dba19487de54d2627cd7eae01b95f5
Louis Chen <cszzys@gmail.com>
2020-12-04 06:04:06 +0000

    update environment

--- ros_cross_compile/docker/build_workspace.sh
+++ ros_cross_compile/docker/build_workspace.sh
@@ -10,5 +10,5 @@ source /opt/ros/"${ROS_DISTRO}"/setup.bash
 set -ux
 colcon build --mixin "${TARGET_ARCH}"-docker \
   --build-base build_"${TARGET_ARCH}" \
-  --install-base install_"${TARGET_ARCH}"
+  --install-base install_"${TARGET_ARCH}" --merge-install
 chown -R "${OWNER_USER}" .
